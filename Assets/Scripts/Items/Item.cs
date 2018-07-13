a979781a9f34e03be7c56aaac1a81ad0caeb578c
JackSnakeMGS4 <jaimerivas81@yahoo.com>
2018-07-12 22:41:48 -0700

    Somewhat lost with the UI code but part of it makes sense; will definitely use this code as a my foundation for UI programming;

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject {
+
+    new public string name = "";
+    public Sprite icon = null;
+    public bool isDefaultItem = false;
+
+    public virtual void Use()
+    {
+        //Use item
+        //Something may happen depending on item used
+
+        Debug.Log("Using " + name);
+    }
+}
