3ab534dccf3f35a922b551b13cda19671f024fe5
Jaime Rivas <jaimerivas81@outlook.com>
2018-09-03 13:08:18 -0700

    Equipment code done and working (definitely use this as a solid starting point)

---
+++ Assets/EquipmentManager.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour {
+
+    #region Singleton
+    public static EquipmentManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    public delegate void OnEquipmentChange(Equipment newItem, Equipment oldItem);
+    public OnEquipmentChange onEquipmentChange;
+
+    Equipment[] currentEquipment;
+    Inventory inventory;
+
+    private void Start()
+    {
+        inventory = Inventory.instance;
+
+        int numOfSlots = System.Enum.GetNames(typeof(EquipSlot)).Length;
+        currentEquipment = new Equipment[numOfSlots];
+    }
+
+    public void Equip(Equipment newItem)
+    {
+        int slotIndex = (int) newItem.equipSlot;
+
+        Equipment oldItem = null;
+        if(currentEquipment[slotIndex] != null)
+        {
+            oldItem = currentEquipment[slotIndex];
+            inventory.AddItem(oldItem);
+        }
+
+        //if  there are methods subscribed to delegate invoke it
+        if(onEquipmentChange != null)
+        {
+            onEquipmentChange.Invoke(newItem, oldItem);
+        }
+
+        currentEquipment[slotIndex] = newItem;
+    }
+
+    public void Unequip(int slotIndex)
+    {
+        if(currentEquipment[slotIndex] != null)
+        {
+            Equipment oldItem = currentEquipment[slotIndex];
+            inventory.AddItem(oldItem);
+
+            currentEquipment[slotIndex] = null;
+
+            //if  there are methods subscribed to delegate invoke it
+            if (onEquipmentChange != null)
+            {
+                onEquipmentChange.Invoke(null, oldItem);
+            }
+        }
+    }
+
+    public void UnequipAll()
+    {
+        for (int i = 0; i < currentEquipment.Length; i++)
+        {
+            Unequip(i);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            UnequipAll();
+        }
+    }
+}
--- Assets/Scripts/Items/Equipment.cs
+++ Assets/Scripts/Items/Equipment.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
 public class Equipment : Item {
-    
+
+    public EquipSlot equipSlot;
+
+    [SerializeField] private int armorMod;
+    [SerializeField] private int dmgMod;
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this);
+        RemoveFromInventory();
+    }
 }
+
+public enum EquipSlot { Head, Body, Legs, Feet, Shield, Weapon}
